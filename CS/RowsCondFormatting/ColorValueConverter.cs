287fa1ffa5382acdd61581ea0d94b9940017b4db
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RowsCondFormatting/ColorValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Media;
+using DevExpress.Xpf.Core.WPFCompatibility;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace RowsCondFormatting {
+    public class ColorValueConverter : IValueConverter {
+
+        public int MaxValue {get; set;}
+
+        public int MinValue {get; set;}
+
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            double ratio = (MaxValue - (int)value) / (double)(MaxValue - MinValue);
+            return new SolidColorBrush(Color.FromArgb(128, 0xff, (byte)(0xAD + (0xff - 0xAD) * ratio), (byte)(0x5D + (0xff - 0x5D) * ratio)));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+        CultureInfo culture) {
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ VB/RowsCondFormatting/ColorValueConverter.vb
@@ -0,0 +1,43 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Media
+Imports DevExpress.Xpf.Core.WPFCompatibility
+Imports System.Windows.Data
+Imports System.Globalization
+
+Namespace RowsCondFormatting
+	Public Class ColorValueConverter
+		Implements IValueConverter
+
+		Private privateMaxValue As Integer
+		Public Property MaxValue() As Integer
+			Get
+				Return privateMaxValue
+			End Get
+			Set(ByVal value As Integer)
+				privateMaxValue = value
+			End Set
+		End Property
+
+		Private privateMinValue As Integer
+		Public Property MinValue() As Integer
+			Get
+				Return privateMinValue
+			End Get
+			Set(ByVal value As Integer)
+				privateMinValue = value
+			End Set
+		End Property
+
+		#Region "IValueConverter Members"
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Dim ratio As Double = (MaxValue - CInt(Fix(value))) / CDbl(MaxValue - MinValue)
+            Return New SolidColorBrush(color.FromArgb(128, &HFF, CByte(&HAD + (&HFF - &HAD) * ratio), CByte(&H5D + (&HFF - &H5D) * ratio)))
+        End Function
+
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Return Nothing
+		End Function
+		#End Region
+	End Class
+End Namespace
